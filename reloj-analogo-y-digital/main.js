41d7de02add85dbcfaf528ff4e25a614bd5de9b6
Haroldx7 <123584394+Haroldx7@users.noreply.github.com>
2023-10-21 16:56:30 -0500

    Add files via upload

---
+++ reloj-analogo-y-digital/main.js
@@ -0,0 +1,35 @@
+let d = document
+
+const horas = d.querySelector('.horas');
+const minutos = d.querySelector('.minutos');
+const segundos = d.querySelector('.segundos');
+
+let gra = 6;
+
+setInterval(()=>{
+    let tiempo = new Date();
+    let hor = tiempo.getHours() * 30;
+    let min = tiempo.getMinutes() * gra;
+    let seg = tiempo.getSeconds() * gra;
+
+    horas.style.transform = `rotate(${(hor)+(min/12)}deg)`;
+    minutos.style.transform = `rotate(${min}deg)`;
+    segundos.style.transform = `rotate(${seg}deg)`;
+});
+
+let digital = d.querySelector('.digital');
+
+setInterval(function(){
+    let tiempo = new Date();
+    let hor = tiempo.getHours();
+    let min = tiempo.getMinutes();
+    let seg = tiempo.getSeconds();
+
+    hor = hor <= 10 ? `0`+hor : hor;
+    min = min <= 10 ? `0`+min : min;
+    seg = seg <= 10 ? `0`+seg : seg;
+
+    let mostrar = `${hor}:${min}:${seg}`;
+
+    digital.innerHTML = mostrar;
+});
